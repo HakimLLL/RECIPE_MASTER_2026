0856b968edc630bd0e312292dab0bfb58e08f2a9
hakim <78786920+HakimLLL@users.noreply.github.com>
2026-08-25 11:34:26 +0200

    Clear database connection constants
    Updated database connection parameters to empty values.

--- app/config/params.php
+++ app/config/params.php
@@ -7,7 +7,7 @@ $content = "Aucun contenu ne correspond à votre requête.";
 // 2. Définir les constantes de connexion 
 //    DB_HOST, DB_NAME, DB_USER, DB_PASSWORD
 
-define("DB_HOST", "localhost:3306");
-define("DB_NAME", "recipe_master_26");
-define("DB_USER", "root");
-define("DB_PASSWORD", "root");
+define("DB_HOST", "");
+define("DB_NAME", "");
+define("DB_USER", "");
+define("DB_PASSWORD", "");
